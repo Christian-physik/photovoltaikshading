afe88527d88f76bb15ac6ec5b3324484fe8301b7
st141178 <st141178@stud.uni-stuttgart.de>
2025-05-07 10:07:49 +0200

    add function for vector calc

--- shadeclass.py
+++ shadeclass.py
@@ -15,6 +15,15 @@ import time
 import datetime
 import os
 
+def length(v1):
+    return(np.sqrt(np.sum(np.square(v1),axis=0)))
+def normalis(v1):
+    return (v1/length(v1))
+def skalar(v1,v2):
+    return(np.sum(v1*v2,axis=0))
+def crosspro(v1,v2):
+    return(np.cross(v1, v2))
+    
 class Rectangle:
     def __init__(self,P_bottomleft,P_bottomright,P_upleft):
         self.orign=P_bottomleft
@@ -28,4 +37,8 @@ class Rectangle:
 p1=np.array((0,0,0))
 p2=np.array((0,1,0))
 p3=np.array((0,0,2))
+v1=np.array((0,3,4))
 rechteck1=Rectangle(p1,p2,p3)
+print('v1length',length(v1))
+print('v1*p3',skalar(v1, p3))
+print('v1xp3',crosspro(v1, p3))
