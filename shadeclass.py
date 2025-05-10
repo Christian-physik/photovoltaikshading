81aac76387b69438d01913b996faca49f6210630
st141178 <st141178@stud.uni-stuttgart.de>
2025-05-10 12:34:45 +0200

    add 4 dim meshgrid

--- shadeclass.py
+++ shadeclass.py
@@ -34,12 +34,52 @@ class Rectangle:
         self.e2=normalis(P_upleft-self.orign)
         
         self.e3=crosspro(self.e1, self.e2)
+class photovoltaikfläche:
+    def __init__(self,rectangle,taxis,datadensity=(3,2),bifa=False):
+        self.rect=rectangle
+        a = np.linspace(0,self.rect.l1,datadensity[0])
+        b = np.linspace(0,self.rect.l2,datadensity[1])
+        i=np.array((0,1,2))
+        A, B = np.meshgrid(a, b)
+        #2Dimensonal
+        print(A)
+        print(B)
+        #ad taxis -> 3 dim t,a,b
+        
+        self.t_tab,self.a_tab, self.b_tab = np.meshgrid(t,a, b,indexing='ij')
+        x_tab=self.rect.orign[0]+ self.a_tab*self.rect.e1[0] + self.b_tab*self.rect.e2[0]
+        y_tab=self.rect.orign[1]+ self.a_tab*self.rect.e1[1] + self.b_tab*self.rect.e2[1]
+        z_tab=self.rect.orign[2]+ self.a_tab*self.rect.e1[2] + self.b_tab*self.rect.e2[2]
+        self.ri_itab=np.stack((x_tab,y_tab,z_tab))
+        
+        print('xyz')
+        print(x_tab)
+        print('y')
+        print(y_tab)
+        print(y_tab[1,0,0])
+        print(y_tab[0,1,0])
+        print(y_tab[0,0,1])
+        print('z')
+        print(z_tab)
+        print(z_tab[1,0,0])
+        print(z_tab[0,1,0])
+        print(z_tab[0,0,1])
+        print('length')
+        print(length(self.ri_itab))
+        
+        #self.i_itab,self.t_itab,self.a_itab, self.b_itab = np.meshgrid(i,t,a, b)
+        #r=orign+a*e1+b*e2
+        #calculate rx(t,a,b),ry(t,a,b)
+        #numpy.stack xyz an reoder so that r_i(i,t,a,b)
+        #heligkeit(t,a,b)
         
 p1=np.array((0,0,0))
 p2=np.array((0,1,0))
-p3=np.array((0,0,2))
+p3=np.array((0,0,1.1))
 v1=np.array((0,3,4))
+t=(np.array((10,11,12,13)))
 rechteck1=Rectangle(p1,p2,p3)
+mesflache1=photovoltaikfläche(rechteck1, t)
 print('v1length',length(v1))
 print('v1*p3',skalar(v1, p3))
 print('v1xp3',crosspro(v1, p3))
